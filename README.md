4b8859ce783ec3808a43d15485338136469a6028
geffen <ncorralbenavente@gmail.com>
2023-02-25 19:35:13 +0100

    First commit, 1.01

---
+++ README.md
@@ -0,0 +1,78 @@
+
+# Project Title
+
+A basic implementation of LINQ methods for Typescript.
+
+
+
+
+## Installation
+
+
+```bash
+  npm install @geffencode/linqscript
+```
+
+    
+## Usage/Examples
+
+```typescript
+import { AsLinq, LINQArray} from "@geffencode/linqscript";
+
+interface Person {
+    id: number;
+    name: string;
+    age: number;
+    city: string;
+    hobbies: string[]
+}
+
+const persons: Person[] = myService.getPersons();
+
+// By declaring a new LINQArray
+const linqArray = new LINQArray<Person>(...persons);
+const names = linqArray.Select(p => p.name); // ['John Doe', 'Jane Doe', 'Jane Smith', ...]
+
+// Copying an existing array as LINQArray
+const ids = AsLinq(persons).Select(p => p.id); // [1, 2, 3, ...]
+
+// Getting the hobbies of people older than 30 for a city
+const hobbies = linqArray.Where(p => p.age > 30 && p.city == 'Mogadishu')
+                                  .SelectMany(p => p.hobbies)
+                                  .Distinct(); // Remove duplicates
+
+// Id, name and age of all people that play competitive chess boxing in Thimphu
+// ordered by age descending, returning a regular array.
+const chessboxers = linqArray.Where(p => p.city === 'Thimphu')
+                            .Select(({id, name, age}) => ({id, name, age}))
+                            .OrderByDescending(p => p.age)
+                            .ToArray();
+
+// Get all persons grouped by city
+const byCity = linqArray.GroupBy(p => p.city);
+
+```
+
+
+## API Reference
+
+
+|Method|Argument|Return|Description|
+|:----|:----|:----|:----|
+|Add|item: T|None|Adds an item to the end of the array.|
+|AddRange|arr: T[]|None|Adds multiple items to the end of the array.|
+|GroupBy|predicate: (item: T) => K|Record<K, T[]>|Groups the elements in the array by the specified key selector.|
+|Select|predicate: (item: T) => K|LINQArray<K>|Projects each element of the array to a new form using the specified selector function.|
+|Where|predicate: (item: T) => boolean|LINQArray<T>|Filters the elements of the array based on a predicate function.|
+|First|predicate?: (item: T) => boolean|T or undefined|Returns the first element of the array that satisfies a specified condition, or undefined if no such element is found.|
+|FirstOrDefault|predicate?: (item: T) => boolean|T or undefined|Returns the first element of the array that satisfies a specified condition, or undefined if no such element is found.|
+|SelectMany|predicate: (item: T) => K[]|LINQArray<K>|Projects each element of the array to a sequence and flattens the resulting sequences into one LINQArray.|
+|Distinct|None|LINQArray<T>|Returns a new LINQArray containing only the distinct elements of the original array.|
+|DistinctBy|predicate: (item: T) => K|LINQArray<T>|Returns a new LINQArray containing only the distinct elements of the original array, determined by the specified key selector function.|
+|OrderBy|predicate: (item: T) => K|LINQArray<T>|Sorts the elements of the array in ascending order according to the specified key selector function.|
+|OrderByDescending|predicate: (item: T) => K|LINQArray<T>|Sorts the elements of the array in descending order based on the specified predicate.|
+|Any|predicate?: (item: T) => boolean|boolean|Determines whether any element of the array satisfies the specified predicate.|
+|Take|i: number|LINQArray<T>|Returns a new array that contains the specified number of elements from the start of the array.|
+|Skip|i: number|LINQArray<T>|Bypasses a specified number of elements in the array and returns the remaining elements.|
+|ToArray|None|T[]|Copies the elements of the LINQArray to a new Array.|
+|_log|None|LINQArray<T>|Logs the current array to the console and returns it.|
---
+++ src/linq.ts
@@ -0,0 +1,180 @@
+/**
+ * A custom implementation of LINQ methods that extend the built-in Array object.
+ * @typeparam T The type of the elements in the LINQArray.
+ */
+class LINQArray<T> extends Array<T> {
+    
+    /**
+     * Logs the current array to the console and returns it.
+     * @returns This instance of the LINQArray.
+     */
+    _log(): LINQArray<T> {
+        console.log(this);
+        return this;
+    }
+    /**
+     * Adds an item to the end of the array.
+     * @param item The item to add to the array.
+     */
+    Add(item: T): void {
+        this.push(item);
+    }
+
+    /**
+     * Adds multiple items to the end of the array.
+     * @param arr The array of items to add to the array.
+     */
+    AddRange(arr: T[]): void {
+        this.push(...arr);
+    }
+
+    /**
+     * Groups the elements in the array by the specified key selector.
+     * @typeparam K The type of the key to group by.
+     * @param predicate The function used to select the grouping key.
+     * @returns An object whose keys are the selected grouping keys and values are arrays of elements that correspond to each key.
+     */
+    GroupBy<K extends string | number | symbol>(predicate: (item: T) => K): Record<K, T[]> {
+        return this.reduce((groups: Record<K, T[]>, item: T) => {
+            const key = predicate(item);
+            if (key in groups)
+              groups[key].push(item);
+            else
+              groups[key] = [item];
+            return groups;
+        }, {} as Record<K, T[]>);
+    }
+
+    /**
+     * Projects each element of the array to a new form using the specified selector function.
+     * @typeparam K The type of the projected element.
+     * @param predicate The function used to project each element of the array.
+     * @returns A new LINQArray containing the projected elements.
+     */
+    Select<K>(predicate: (item: T) => K): LINQArray<K>{
+        return this.map(predicate) as LINQArray<K>;
+    }
+
+    /**
+     * Filters the elements of the array based on a predicate function.
+     * @param predicate The function used to test each element of the array.
+     * @returns A new LINQArray containing the elements that pass the test.
+     */
+    Where(predicate: (item: T) => boolean): LINQArray<T> {
+        return this.filter(predicate) as LINQArray<T>;
+    }
+
+    /**
+     * Returns the first element of the array that satisfies a specified condition, or undefined if no such element is found.
+     * @param predicate The function used to test each element of the array.
+     * @returns The first element that satisfies the condition, or undefined if no such element is found.
+     */
+    First(predicate?: (item: T) => boolean): T {
+        const item = this.FirstOrDefault(predicate);
+        if (item === undefined) throw new InvalidOperationException('No element satisfies the condition in predicate.');
+        return item;
+    }
+
+    /**
+     * Returns the first element of the array that satisfies a specified condition, or undefined if no such element is found.
+     * @param predicate The function used to test each element of the array.
+     * @returns The first element that satisfies the condition, or undefined if no such element is found.
+     */
+    FirstOrDefault(predicate?: (item: T) => boolean): T | undefined {
+        return predicate ? this.find(predicate) : this.length ? this[0] : undefined;
+    }
+
+    /**
+     * Projects each element of the array to a sequence and flattens the resulting sequences into one LINQArray.
+     * @typeparam K The type of the elements in the resulting sequence.
+     * @param predicate The function used to project each element of the array to a sequence.
+     * @returns A new LINQArray containing the flattened sequence of elements.
+     */
+    SelectMany<K>(predicate: (item: T) => K[]): LINQArray<K> {
+        return this.flatMap(predicate) as LINQArray<K>;
+    }
+    
+    /**
+     * Returns a new LINQArray containing only the distinct elements of the original array.
+     * @returns A new LINQArray containing only the distinct elements of the original array.
+     */
+    Distinct(): LINQArray<T> {
+        return [...new Set(this)] as LINQArray<T>
+    }
+
+    /**
+     * Returns a new LINQArray containing only the distinct elements of the original array, determined by the specified key selector function.
+     * @typeparam K The type of the key to use for distinctness.
+     * @param predicate The function used to select the key for distinctness.
+     * @returns A new LINQArray containing only the distinct elements of the original array.
+     */
+    DistinctBy<K>(predicate: (item: T) => K): LINQArray<T> {
+        return AsLinq([...new Map(this.map(i => [predicate(i), i])).values()]);
+    }
+
+    /**
+     * Sorts the elements of the array in ascending order according to the specified key selector function.
+     * @typeparam K The type of the key used for sorting.
+     * @param predicate The function used to select the key for sorting.
+     * @returns A new LINQArray containing the sorted elements.
+     */
+    OrderBy<K>(predicate: (item: T) => K): LINQArray<T> {
+        return this.sort((a, b) => predicate(a) > predicate(b) ? -1 : 1 );
+    }
+
+    /**
+     * Sorts the elements of the array in descending order based on the specified predicate.
+     * @typeparam K The type of the key returned by the predicate.
+     * @param predicate A function that returns the key used to sort the elements.
+     * @returns A new LINQArray instance with the sorted elements.
+     */
+    OrderByDescending<K>(predicate: (item: T) => K): LINQArray<T> {
+        return this.sort((a, b) => predicate(a) > predicate(b) ? 1 : -1 );
+    }
+
+    /**
+     * Determines whether any element of the array satisfies the specified predicate.
+     * @param predicate A function that determines whether an element meets a condition.
+     * @returns True if any element satisfies the predicate; otherwise, false.
+     */
+    Any(predicate?: (item: T) => boolean): boolean {
+        return predicate ? this.some(predicate) : this.length > 0;
+    }
+
+    /**
+     * Returns a new array that contains the specified number of elements from the start of the array.
+     * @param i The number of elements to take.
+     * @returns A new LINQArray instance that contains the specified number of elements.
+     */
+    Take(i: number): LINQArray<T> {
+        return this.slice(0, i) as LINQArray<T>;
+    }
+
+    /**
+     * Bypasses a specified number of elements in the array and returns the remaining elements.
+     * @param i The number of elements to skip.
+     * @returns A new LINQArray instance that contains the remaining elements.
+     */
+    Skip(i: number): LINQArray<T> {
+        return this.slice(i, this.length) as LINQArray<T>;
+    }
+
+    /**
+     * Copies the elements of the LINQArray to a new Array.
+     * @returns A new array that contains the same elements as the LINQ array.
+     */
+    ToArray(): T[] {
+        return [...this];
+    }
+
+}
+
+class InvalidOperationException extends Error {
+
+}
+
+const AsLinq = <T>(arr: T[]): LINQArray<T> => {
+    return new LINQArray<T>(...arr);
+}
+
+export {AsLinq, LINQArray}
